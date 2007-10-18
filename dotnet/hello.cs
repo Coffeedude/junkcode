905cb1036f88cd7cce88867a102d6224fa13576c
Jerry Carter <jerry@plainjoe.org>
2007-10-18 12:32:01 -0500

    Simple HelloWorld C# example.
    Copied from the book "Open Source .NET Development" by Brian Nantz.

---
+++ dotnet/hello.cs
@@ -0,0 +1,12 @@
+using System;
+namespace Simple
+{
+     class HelloWorld
+     {
+     [STAThread]
+     static void Main(string[] args)
+     {
+         Console.WriteLine("Hello World.");
+     }
+   }
+}
